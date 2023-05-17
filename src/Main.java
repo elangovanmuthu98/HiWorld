f6fe1e4c8710fe7cce52c8f3a7d7b7fcec25f702
evildoomsizer <evildoomsizer@gmail.com>
2023-05-17 08:05:56 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+      hi n=new hi();
+    }
+}
+class hi
+{
+    hi()
+    {
+        System.out.println("Hi");
+    }
+}
